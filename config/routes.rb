4d5f5590f4595ee28650e3c82fd2458ba91ad530
eatplaynap <eatplaynap329@gmail.com>
2021-07-08 15:01:58 +0900

    ルーティングでロケールの使用を強制させない処理追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "/:locale" do
+  scope "(:locale)" do
     resources :books
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
