3227cfe153e196e990751e65b72ddcf90ca7d5c1
eatplaynap <eatplaynap329@gmail.com>
2021-07-09 11:24:56 +0900

    データの並び順を指定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.page(params[:page]).per(3)
-    # @books = Book.all
+    @books = Book.order(:id).page(params[:page]).per(3)
   end
 
   # GET /books/1
