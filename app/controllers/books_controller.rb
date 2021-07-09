34ffcd3baa4a5264326fe5b85aca139e8d72f1e7
eatplaynap <eatplaynap329@gmail.com>
2021-07-09 11:20:47 +0900

    kaminari実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(3)
+    # @books = Book.all
   end
 
   # GET /books/1
