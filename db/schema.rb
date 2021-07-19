2902eda47d313ad34be73a2a152c6e342c8751a3
eatplaynap <eatplaynap329@gmail.com>
2021-07-19 13:59:06 +0900

    必要なカラムを全て追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_014320) do
+ActiveRecord::Schema.define(version: 2021_07_19_044751) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2021_07_19_014320) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "postal_code"
+    t.string "name"
+    t.text "address"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
