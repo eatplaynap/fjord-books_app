ccf9cbdf39f8f4e79bee71053a3d0e1684206e5f
eatplaynap <eatplaynap329@gmail.com>
2021-07-21 14:46:45 +0900

    初期フォーム以外も登録、編集できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
     books_path
@@ -11,4 +12,10 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :profile])
+  end
 end
