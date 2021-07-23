e1fa7df8b2e024d06645cc4a0354b3ca92da1945
eatplaynap <eatplaynap329@gmail.com>
2021-07-23 21:43:48 +0900

    letter_opener実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :books
 
   root 'books#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
