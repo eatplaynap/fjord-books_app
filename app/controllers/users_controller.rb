6293f25f30780d2784ea0132f15d55a921b817c8
eatplaynap <eatplaynap329@gmail.com>
2021-08-11 17:21:54 +0900

    必要のないアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to root_url, notice: 'Add User'
-    else
-      render :new
-    end
-  end
-
   private
 
   def user_params
