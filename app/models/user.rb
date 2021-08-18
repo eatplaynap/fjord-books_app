71540a7b2339c376f025e38f134fa8eccca42f01
eatplaynap <eatplaynap329@gmail.com>
2021-08-18 14:42:12 +0900

    Userモデルにfollowingの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
            foreign_key: "follower_id",
            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 end
