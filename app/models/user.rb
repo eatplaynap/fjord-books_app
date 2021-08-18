d4364cd27466f6a346a33b7ccf4b0dd9ba350f97
eatplaynap <eatplaynap329@gmail.com>
2021-08-18 14:48:33 +0900

    "following" 関連のメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
            foreign_key: "follower_id",
            dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
