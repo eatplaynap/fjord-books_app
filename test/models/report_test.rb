35684883ecc44fbc59d5eadb306b1fee62b5ecaa
eatplaynap <eatplaynap329@gmail.com>
2021-09-14 22:57:51 +0900

    rubocop OK

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -12,6 +12,6 @@ class ReportTest < ActiveSupport::TestCase
   test '#created_on' do
     user = User.create!(email: 'hoge@example.com', password: 'password')
     report = Report.create!(user_id: user.id, title: 'Great Day', content: 'Everything is awesome.', created_at: '2021/09/14')
-    assert_equal Date.new(2021,9,14), report.created_on
+    assert_equal Date.new(2021, 9, 14), report.created_on
   end
 end
